d69f457c7cf2edd4e15adf9675a911003b3de2a0
Asis Patra <asis.patra0425@gmail.com>
2020-06-25 14:18:57 -0400

    Commit tried

--- benchmarks/init.sh
+++ benchmarks/init.sh
@@ -2,6 +2,7 @@
 
 # Allow PORT in firewall
 echo "Allow PORT 9999"
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 firewall-cmd --zone=public --add-port=9999/tcp --permanent 
 firewall-cmd --reload
 
