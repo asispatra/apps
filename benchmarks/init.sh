06e9b8d2c31574f54a0c656477ffd38637fc1464
Asis Patra <asis.patra0425@gmail.com>
2020-06-25 14:49:03 -0400

    schbench and fio

--- benchmarks/init.sh
+++ benchmarks/init.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Allow PORT in firewall
-echo "Allow PORT 9999"
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-firewall-cmd --zone=public --add-port=9999/tcp --permanent 
-firewall-cmd --reload
-
 python3 -u server.py
 
