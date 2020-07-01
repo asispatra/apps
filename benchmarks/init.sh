256db3c6aef512dcfa00311b420510772aa1ae79
Asis Patra <asis.patra0425@gmail.com>
2020-07-01 12:02:14 -0400

    Commit tried

--- benchmarks/init.sh
+++ benchmarks/init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+sshpass -p 'passw0rd' scp -r root@ocpbastion.aus.stglabs.ibm.com:/root/asis/scripts/ .
+cd scripts/schbench/schbench
+make
+bash run.sh
+sshpass -p 'passw0rd' scp -r logs root@ocpbastion.aus.stglabs.ibm.com:/root/asis/results
+cd
 python3 -u server.py
 
