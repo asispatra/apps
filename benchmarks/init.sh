4ca3e7d3f42caec3bb078e38cc22da2aa3b4c52b
Asis Patra <asis.patra0425@gmail.com>
2020-07-01 13:19:04 -0400

    Commit tried

--- benchmarks/init.sh
+++ benchmarks/init.sh
@@ -2,6 +2,6 @@
 
 cd schbench/schbench
 wget "https://raw.githubusercontent.com/asispatra/asisapps/master/benchmarks/download_run_scripts.sh" -O - | bash
-bash run_schbench.sh
+taskset -a -c 0-7 bash run_schbench.sh
 python3 -u server.py
 
